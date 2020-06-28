3350e556c146e9d89b08f1b1e469ddd2dbc92af4
Juhi Chandalia <jkchandalia@gmail.com>
2020-06-27 18:07:34 -0700

    Find comments for Reddit page structure. Jsonify array.

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,3 +1,5 @@
+//TO DO: need to find way to grab only relevant text. Limit to say reddit. 
+
 alert("Generating summary highlights. This may take up to 30 seconds depending on length of article.");
 
 function unicodeToChar(text) {
@@ -9,18 +11,47 @@ function unicodeToChar(text) {
 
 // capture all text
 var textToSend = document.body.innerText;
+console.log(typeof(textToSend));
+var realText = document.querySelectorAll("[data-test-id='comment']")
+//console.log(realText[0]);
+//console.log(realText.length);
+var counter = 0;
+for (let i = 0; i<realText.length; i++) {
+	//console.log(realText[i]);
+	var comments = realText[i].getElementsByTagName('p');
+	//console.log(comments);
+	for (let j = 0; j<comments.length; j++){
+		counter++;
+		console.log(comments[j].innerHTML);
+	}
+}
 
-console.log(typeof(textToSend.split("\n")));
+var commentArray = new Array(counter);
+counter=0;
+for (let i = 0; i<realText.length; i++) {
+	//console.log(realText[i]);
+	var comments = realText[i].getElementsByTagName('p');
+	//console.log(comments);
+	for (let j = 0; j<comments.length; j++){
+		commentArray[counter] = comments[j].innerHTML;
+		counter++;
+		console.log(comments[j].innerHTML);
+	}
+}
+
+alert(JSON.stringify(commentArray));
+
+console.log(realText);
 var textArray = textToSend.split("\n");
 
 for (let i = 0; i < 30; i++) {
 	var value = textArray[i];
-	console.log("value");
-	console.log(value);
-	console.log("html split");
-	console.log(document.body.innerHTML.split(value));
-	if (value){
-	document.body.innerHTML = document.body.innerHTML.split(value).join('<span style="background-color: #fff799;">' + value + '</span>');}
+	//console.log("value");
+	//console.log(value);
+	//console.log("html split");
+	//console.log(document.body.innerHTML.split(value));
+	//if (value){
+	//document.body.innerHTML = document.body.innerHTML.split(value).join('<span style="background-color: #fff799;">' + value + '</span>');}
   }
   
 /* for (const value in textToSend.split("\n")) {
@@ -33,7 +64,7 @@ for (let i = 0; i < 30; i++) {
 
 
 // summarize and send back
-const api_url = 'YOUR_GOOGLE_CLOUD_FUNCTION_URL';
+//const api_url = 'YOUR_GOOGLE_CLOUD_FUNCTION_URL';
 
 /* fetch(api_url, {
   method: 'POST',

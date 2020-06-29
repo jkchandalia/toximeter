af67071c03664820b92fffcc45595aeaaa7cc7a5
Juhi Chandalia <jkchandalia@gmail.com>
2020-06-29 01:29:31 -0700

    Reference local jquery script

--- chrome_extension/popup2.js
+++ chrome_extension/popup2.js
@@ -1,5 +1,5 @@
 function summarize() {
-	chrome.tabs.executeScript(null, { file: "jquery-2.2.js" }, function() {
+	chrome.tabs.executeScript(null, { file: "jquery-2.2.4.min.js" }, function() {
 	    chrome.tabs.executeScript(null, { file: "content.js" });
 	});
 }

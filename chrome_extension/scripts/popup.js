704c132e942a358eb7adcefdc257dba20d9e4ca8
Juhi Chandalia <jkchandalia@gmail.com>
2020-07-06 16:14:26 -0700

    Run twitter toxicity periodically, remove reddit extension

--- chrome_extension/scripts/popup.js
+++
@@ -1,6 +0,0 @@
-function analyze_toxicity() {
-	chrome.tabs.executeScript(null, { file: "scripts/jquery-2.2.4.min.js" }, function() {
-	    chrome.tabs.executeScript(null, { file: "scripts/content.js" });
-	});
-}
-document.getElementById('clickme').addEventListener('click', analyze_toxicity);
---
+++ chrome_extension_twitter/scripts/content.js
@@ -0,0 +1,53 @@
+//TO DO: need to find way to grab only relevant text. Limit to say twitter. 
+
+
+function unicodeToChar(text) {
+	return text.replace(/\\u[\dA-F]{4}/gi, 
+	      function (match) {
+	           return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+	      });
+}
+
+var process_new_tweets = function() {
+	if(typeof(api_url) === 'undefined' ) {
+		api_url = 'https://us-central1-toxicity-90.cloudfunctions.net/get_toxicity_prediction';
+	};
+
+	var tweetText = document.querySelectorAll("[data-testid='tweet']")
+	var counter = tweetText.length;
+	var commentArray = new Array(counter);
+	for (let i = 0; i<tweetText.length; i++) {
+		var comments = tweetText[i].getElementsByTagName('span');
+		var commentText = '';
+		for (let j = 0; j<comments.length; j++){
+			commentText = commentText.concat(' ');
+			commentText = commentText.concat(comments[j].innerText);}
+			commentArray[i]=commentText;
+	}
+	fetch(api_url, {
+		method: 'POST',
+		body: JSON.stringify(commentArray),
+		headers:{
+		  'Content-Type': 'application/json'
+		} })
+	  .then(data => { return data.json() })
+	  .then(res => { 
+		  $.each(res, function( index, value ) {
+			  //value = unicodeToChar(value).replace(/\\n/g, '');
+			  score = value[1];
+			  comment_text = value[0];
+			  console.log(value);
+			  if (score>.7){
+			  tweetText[index].style.backgroundColor="red";}
+		  });
+	   })
+	  .catch(error => console.log('Error:', error));
+	}
+	
+	setTimeout(process_new_tweets, 1000); // scan timeline 1sec after load
+	setInterval(process_new_tweets, 5000); // every 10 sec lets scan all
+
+
+
+
+ 
